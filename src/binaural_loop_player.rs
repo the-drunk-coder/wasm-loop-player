86fef443046ce9ca9f10252e832906f3855d11c5
nik <nik@parkellipsen.de>
2019-06-08 17:00:35 +0200

    corrections

--- src/binaural_loop_player.rs
+++ src/binaural_loop_player.rs
@@ -23,15 +23,16 @@ impl BinauralLoopPlayer {
     
     pub fn process(&mut self, out_ptr_l: *mut f32, out_ptr_r: *mut f32, size: usize) {
         let buf = self.player.get_next_block();
+        let buf_ambi = self.encoder.encode_block(&buf, 0.0, 0.0);
+        let buf_bin = self.binauralizer.binauralize(&buf_ambi);
         
         let out_buf: &mut [f32] = unsafe { std::slice::from_raw_parts_mut(out_ptr_l, size)};
         for i in 0..size {
-            out_buf[i] = buf[i] as f32;
+            out_buf[i] = buf_bin[0][i] as f32;
         }
     }
 
-    pub fn set_sample_data_raw(&mut self, in_ptr: *mut f32, sample_size: usize) {
-        
+    pub fn set_sample_data_raw(&mut self, in_ptr: *mut f32, sample_size: usize) {        
         let in_buf: &mut [f32] = unsafe { std::slice::from_raw_parts_mut(in_ptr, sample_size)};
         self.player.set_loop(&in_buf.to_vec());
     }
--- src/binaural_loop_player/binauralizer.rs
+++ src/binaural_loop_player/binauralizer.rs
@@ -24,15 +24,15 @@ impl Binauralizer {
         }            
     }
 
-    pub fn binauralize(&mut self, input: [[f32; 128]; 4]) -> [[f32; 128]; 2] {
+    pub fn binauralize(&mut self, input: &[[f32; 128]; 4]) -> [[f32; 128]; 2] {
         let mut bin_block = [[0.0; 128]; 2];
                                     
         for ach in 0..4 {
             let lch = self.left[ach].convolve(&input[ach]);
             let rch = self.right[ach].convolve(&input[ach]);
             for fr in 0..128 {                               
-                bin_block[0][fr] = lch[0] + lch[1] + lch[2] + lch[3];
-                bin_block[1][fr] = rch[0] + rch[1] + rch[2] + rch[3];
+                bin_block[0][fr] += lch[fr];
+                bin_block[1][fr] += rch[fr];
             }
         }
 
