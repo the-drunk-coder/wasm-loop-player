a91da8752fc34e287e63de4d71bc9d9aa025152a
nik <nik@parkellipsen.de>
2019-11-03 23:44:46 +0100

    some more explanatory comments

--- js/setup.js
+++ js/setup.js
@@ -1,17 +1,34 @@
+// Get the audio context.
+// Omitting the low latency hint for simplicity.
 const ctx = new AudioContext({
   sampleRate: 44100,
 })
 
+// Not all Browsers currently support the AudioWorklet.
 if (ctx.audioWorklet === undefined) {
     alert("AudioWorklet isn't supported... It cannot work.")
 } else {  
+    // First, load the AudioWorklet module.
     ctx.audioWorklet.addModule('js/worklet.js?t=' + new Date().getTime())
 	.then(() => {
+
+	    ///////////////////////////////
+	    // BASIC WEB AUDIO API SETUP //
+	    ///////////////////////////////	    
+	    
+	    // once the module has been loaded, create the DSP graph
 	    const n = new AudioWorkletNode(ctx, 'loop-player-processor', { numberOfInputs: 1,
 									   numberOfOutputs: 1,
 									   outputChannelCount: [2], } );
+	    // Activate the node by connecting it to the output.
 	    n.connect(ctx.destination)
 
+	    //////////////////
+	    // GUI ELEMENTS //
+	    //////////////////
+	    
+	    // This button activates or deactivates the sample player.
+	    // Automatic playback is not allowed.
 	    const autoPlay = document.getElementById('auto-play')
 	    autoPlay.addEventListener('change', e => {
 		if (e.target.value === 0) {
@@ -20,35 +37,53 @@ if (ctx.audioWorklet === undefined) {
 		    if(ctx.state === "suspended") {
 			ctx.resume();
 		    }
-
 		    n.port.postMessage({ type: 'enable', })
 		}
 	    })
-	    
+
+	    // Azimuth slider.
 	    const azi = document.getElementById('azimuth-slider')
 	    azi.addEventListener('input', e => {
 		n.parameters.get('azimuth').value = e.target.value
 	    })
-	    
+
+	    // Elevation slider.
 	    const ele = document.getElementById('elevation-slider')
 	    ele.addEventListener('input', e => {
 		n.parameters.get('elevation').value = e.target.value
 	    })
-	    
+
+	    //////////////////////////////
+	    // ADDITIONAL WORKLET SETUP //
+	    //////////////////////////////
+	   
+	    // Here's one of the crucial challenges. The WebAudio API currently doesn't              
+	    // allow any fetch(...) calls in the worklet module itself.
+	    //
+	    // This is why we need to fetch everything in the main thread and post it (as binary data) 
+	    // to the worklet using the worklet using the message port facilities.
+	    //
+	    // This doesn't only include the sample files, but also the WebAssembly module itself.
+	    //
+	    // In the worklet you'll find the other end of the message port.
+
+	    // Fetch the actual sample.
 	    fetch('audio/amen.flac?t=' + new Date().getTime())
 		.then(r => r.arrayBuffer())
 		.then(r => ctx.decodeAudioData(r)
 		      .then(r => n.port.postMessage({ type: 'loadSample', samples: r.getChannelData(0), length: r.length })))
-	    
+
+	    // Fetch the impulse response we need for the binaural processing.
 	    fetch('audio/ir.flac?t=' + new Date().getTime())
 		.then(r => r.arrayBuffer())
 		.then(r => ctx.decodeAudioData(r)
 		      .then(r => n.port.postMessage({ type: 'loadIr', samples: r.getChannelData(0), length: r.length })))
-	    
+
+	    // Fetch the WebAssembly module which contains our main DSP logic.
 	    fetch('wasm/wasm_loop_player.wasm?t=' + new Date().getTime())
 		.then(r => r.arrayBuffer())
 		.then(r => n.port.postMessage({ type: 'loadWasm', data: r }))	    
 	})
-   
-    console.log("sr: " + ctx.sampleRate);    
+    // If successful, post samplerate ...
+    console.log("Samplerate: " + ctx.sampleRate);    
 }
--- js/worklet.js
+++ js/worklet.js
@@ -1,4 +1,5 @@
 class LoopPlayerProcessor extends AudioWorkletProcessor {
+    // The parameter descriptors are part of the API standard.
     static get parameterDescriptors() {
 	return [
 	    {
@@ -14,32 +15,51 @@ class LoopPlayerProcessor extends AudioWorkletProcessor {
     
     constructor(options) {
 	super(options)
+	// Set up the receiving part of the worklet's message port.
+	// Use the 'data.type' field to define the handling.
 	this.port.onmessage = e => {
-	    // unfortunately, this seems to be the only way to load
-	    // the wasm module in the worklet.
-	    // we have to fetch it here because the worklet scope doesn't expose
-	    // 'fetch()'
-	    if (e.data.type === 'loadWasm') {		
-		WebAssembly.instantiate(e.data.data).then(w => {		    
+	    // Recieve the WebAssembly module (the Rust part) as binary data.
+	    // In the setup.js part there's some explanation why this is necessary.
+	    // Basically, 'fetch()' calls aren't allowed in the worklet itself.
+	    if (e.data.type === 'loadWasm') {
+
+		// Instantiate the WebAssembly module from the received binary data.
+		WebAssembly.instantiate(e.data.data).then(w => {
+
+		    // Call wasm module constructor.
 		    this._wasm = w.instance
-		    // grow memory to accomodate full sample ... 
+
+		    // Grow memory to accomodate full sample.
 		    this._wasm.exports.memory.grow(250)
+
+		    // Set the blocksize.
 		    this._size = 128
-		    // WHY IS THE ALLOCATION ORDER IMPORTANT HERE ??
-		    // BYTE OFFSET ??
+
+		    // If the sample data has already been received (the order isn't necessarily
+		    // deterministic), allocate some memory in the linear WebAssembly
+		    // memory to hold ths sample and share it with the WebAssembly module.
 		    if(this._sample_data) {
+
+			// Receive the pointer.
 			this._samplePtr = this._wasm.exports.alloc(this._sample_size)	
+			// Create the JS Array in that memory location.
 			this._sampleBuf = new Float32Array (
 			    this._wasm.exports.memory.buffer,
 			    this._samplePtr,
 			    this._sample_size
 			)
 			
-			// copy to wasm buffer
+			// Copy the sample data to the shared space.
 			this._sampleBuf.set(this._sample_data);
+			
+			// Tell the WebAssembly module which sample to use.
 			this._wasm.exports.set_sample_data_raw(this._samplePtr, this._sample_size)
+
+			// Flag for the processing method.
 			this._sample_set = true;
 		    }
+
+		    // Same procedure as above, just for the impulse response.
 		    if(this._ir_data) {
 			this._irPtr = this._wasm.exports.alloc(this._ir_size)	
 			this._irBuf = new Float32Array (
@@ -47,12 +67,13 @@ class LoopPlayerProcessor extends AudioWorkletProcessor {
 			    this._irPtr,
 			    this._ir_size
 			)
-			
-			// copy to wasm buffer
+					
 			this._irBuf.set(this._ir_data);
 			this._wasm.exports.set_ir_data_raw(this._irPtr, this._ir_size)
 			this._ir_set = true;
 		    }
+
+		    // And once more, for the output buffers.
 		    this._outPtr_r = this._wasm.exports.alloc(this._size)		    
 		    this._outBuf_r = new Float32Array (
 			this._wasm.exports.memory.buffer,
@@ -65,12 +86,15 @@ class LoopPlayerProcessor extends AudioWorkletProcessor {
 			this._outPtr_l,
 			this._size
 		    )
-		})		
+		})	
 	    } else if (e.data.type === 'loadSample') {		
-		// same with the sample ... 
+		// Load the sample date from binary data received over the message port.
 		this._sample_size = e.data.length
 		this._sample_data = e.data.samples
 		console.log("sample size: " + this._sample_data.length);
+
+		// If the WebAssembly module is already loaded (the order isn't necessarily
+		// deterministic), allocate buffer space (see explanation above).
 		if(this._wasm) {
 		    this._samplePtr = this._wasm.exports.alloc(this._sample_size)	
 		    this._sampleBuf = new Float32Array (
@@ -85,10 +109,13 @@ class LoopPlayerProcessor extends AudioWorkletProcessor {
 		    this._sample_set = true;
 		}
 	    } else if (e.data.type === 'loadIr') {
-		// same with the sample ... 
+		// Load the sample date from binary data received over the message port.
 		this._ir_size = e.data.length
 		this._ir_data = e.data.samples
 		console.log("ir size: " + this._ir_data.length);
+		
+		// If the WebAssembly module is already loaded (the order isn't necessarily
+		// deterministic), allocate buffer space (see explanation above).
 		if(this._wasm) {
 		    this._irPtr = this._wasm.exports.alloc(this._ir_size)	
 		    this._irBuf = new Float32Array (
@@ -101,12 +128,14 @@ class LoopPlayerProcessor extends AudioWorkletProcessor {
 		    // load to loop player
 		    this._wasm.exports.set_ir_data_raw(this._irPtr, this._ir_size)
 		    this._ir_set = true;
-		}
+		}		
 	    } else if (e.data.type === 'enable') {
+		// Enable playing if WebAssembly module is enabled.
 		if(this._wasm) {
 		    this._wasm.exports.enable();
 		}
 	    } else if (e.data.type === 'disable') {
+		// Disable playing if WebAssembly module is enabled.
 		if(this._wasm) {
 		    this._wasm.exports.disable();
 		}
@@ -119,11 +148,16 @@ class LoopPlayerProcessor extends AudioWorkletProcessor {
 	    return true
 	}
 
+	// Set parameters
 	this._wasm.exports.set_azimuth(parameters.azimuth[0])
 	this._wasm.exports.set_elevation(parameters.elevation[0])
-	
+	// get the output buffer (DeInterleaved)
 	let output = outputs[0];
+
+	// Process ...
 	this._wasm.exports.process(this._outPtr_l, this._outPtr_r, this._size)
+
+	// Set output buffers.
 	output[0].set(this._outBuf_l)
 	output[1].set(this._outBuf_r)
 
@@ -131,4 +165,5 @@ class LoopPlayerProcessor extends AudioWorkletProcessor {
     }
 }
 
+// Register processor module.
 registerProcessor('loop-player-processor', LoopPlayerProcessor)
